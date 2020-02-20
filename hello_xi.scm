0e255bff704483fbc8c4e7e8f576defcf9098786
Gerry Agbobada <gagbobada+git@gmail.com>
2020-02-21 00:42:43 +0100

    Add basic JSON parsing

--- hello_xi.scm
+++ hello_xi.scm
@@ -4,20 +4,56 @@
 (use-modules (ice-9 popen)
              (ice-9 threads)
              ;; (ice-9 getopt-long) ;; for CLI options
-             (ice-9 rdelim))
+             (ice-9 rdelim)
+             (json))
 
+;; Debugging / Logging
+(define (xile--debug severity message)
+  (write-line (string-append severity ": "  message) (current-error-port)))
+
+(define (xile--debug-info message)
+  (xile--debug "INFO" message))
+
+;; Xile protocol messages serialization / sending
+(define (xile--msg-init)
+  (scm->json-string '((method . "client_started") (params))))
+
+(define (xile--msg-send port message)
+  (xile--debug-info (string-append "Sending : " message))
+  (write-line message port))
+
+;; Xile protocol messages deserialization / receiving
+(define (xile--msg-read port)
+  (when (char-ready? port)
+    (xile--debug-info "reading one line")
+    (xile--msg-dispatch (json->scm port))))
+
+;; Here the message is parsed JSON. To assert
+(define (xile--msg-dispatch message)
+  (write-line message (current-error-port))
+  )
+
+(define (xile--msg-handler port)
+  (while (not (port-closed? port))
+    (xile--msg-read port)))
+
+
+;; Main
 (define (main args)
-  (let ((xi-proc (open-input-output-pipe "xi-core")))
-    ;; Sender "thread"
-    (display "{\"method\":\"client_started\",\"params\":{}}\n" xi-proc)
+  (let* ((xi-proc (open-input-output-pipe "xi-core"))
+         (init-client (xile--msg-init))
+         (listener (make-thread xile--msg-handler xi-proc)))
 
-    ;; Listener thread
-    (call-with-new-thread
-     (while #t
-       (write (read-line xi-proc))
-       (newline)))
+    ;; Flush I/O with Xi on each line
+    (setvbuf xi-proc 'line)
 
-    ;; TODO : event loop thread
+    ;; Init code
+    (xile--msg-send xi-proc init-client)
 
-    (close-pipe xi-proc)
-    ))
+    ;; TODO : event loop thread instead of joining without timeout
+    (usleep 500000)
+    ;; TODO : Understand why listener thread doesn't die here
+    (write-line "Because of a bug, you need to Control-C to quit for now")
+    (cancel-thread listener)
+
+    (close-pipe xi-proc)))
