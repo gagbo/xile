8d630fdc5a245d46118d93676880a46c6a6496f7
Gerry Agbobada <gagbobada+git@gmail.com>
2020-02-26 21:58:47 +0100

    Add tests reporting

--- test/xile--msg-spec.scm
+++ test/xile--msg-spec.scm
@@ -4,12 +4,14 @@
 (load "check.scm")
 (load "../xile.scm")
 
-(use-modules (check))
+(use-modules (check)
+             (ice-9 pretty-print))
+(define test-errors '())
 
+;;; xile : Messages Tests
 (check (xile--msg-init '()) => "{\"method\":\"client_started\",\"params\":{}}" )
 (check (xile--msg-init '((config_dir . "test_path/togo"))) => "{\"method\":\"client_started\",\"params\":{\"config_dir\":\"test_path/togo\"}}" )
 
-;; TODO : check expected values of the test
 (check (xile--msg-new_view '((file_path . "test_path/xile.scm"))) => "{\"id\":0,\"method\":\"new_view\",\"params\":{\"file_path\":\"test_path/xile.scm\"}}" )
 
 (check (xile--msg-plugin-start "test_view" "syntect") => "{\"id\":0,\"method\":\"plugin\",\"params\":{\"method\":\"start\",\"params\":{\"view_id\":\"test_view\",\"plugin_name\":\"syntect\"}}}")
@@ -17,3 +19,6 @@
 (check (xile--msg-edit-insert "test_view" '()) => "{\"id\":0,\"method\":\"edit\",\"params\":{\"method\":\"insert\",\"view_id\":\"test_view\",\"params\":{}}}")
 (check (xile--msg-edit-paste "test_view" '((chars . "password"))) => "{\"id\":0,\"method\":\"edit\",\"params\":{\"method\":\"paste\",\"view_id\":\"test_view\",\"params\":{\"chars\":\"password\"}}}")
 (check (xile--msg-edit-move_right "test_view") => "{\"id\":0,\"method\":\"edit\",\"params\":{\"method\":\"move_right\",\"view_id\":\"test_view\",\"params\":{}}}")
+
+(check-report)
+(exit (if (and (= (length test-errors) 0) (= 0 (length check:failed))) 0 1))
