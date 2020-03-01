005461ba665094370e3bc4ad9ce904c6fbc02167
Gerry Agbobada <10496163+gagbo@users.noreply.github.com>
2020-03-01 23:04:02 +0100

    [WIP] Messaging layer from/to Xi (#6)
    * Add Front to Back messages
    
    Next step is tests (source is
    https://xi-editor.io/docs/frontend-protocol.html )
    , and then back to front tests and messages
    
    * Add test file using check and message unit-tests
    
    * Refactor : move tests in the correct folder
    
    * Rename xile
    
    * Add TODO
    
    * Fix tests
    
    * Add tests reporting
    
    * Implement incrementing id
    
    * Move dispatch and send into the "RPC layer environment"
    
    * Fix message sending
    
    * Open a new view to see responses
    
    * Add hashtable for callbacks hopefully
    
    * Add data to work on
    
    * Rubber duck next step
    
    * Add source for the tests
    
    * Add basic callback support
    
    * Use (format) to handle logging/printing
    
    * Rename the xile-methods
    
    * Reorder files using modules

---
+++ xile.scm
@@ -0,0 +1,49 @@
+#!/usr/bin/env -S guile -e main -s
+!#
+;; coding: utf-8
+(add-to-load-path (dirname (current-filename)))
+(use-modules (ncurses curses)
+             (ice-9 threads)            ;; For the join-thread in main
+             (xile message)
+             (xile json-rpc))
+
+;; Main
+(define (main args)
+  (let* ((xi-setup (xile-setup))
+         (port-from-xi (cadr xi-setup))
+         (port-to-xi (cddr xi-setup))
+         (listener (car xi-setup)))
+
+    (set-current-error-port (open "logs/xile-err.log" (logior O_APPEND O_CREAT O_WRONLY)))
+    (set-current-output-port (open "logs/xile-out.log" (logior O_APPEND O_CREAT O_WRONLY)))
+
+    ;; (define stdscr (initscr))
+    ;; (raw!)
+    ;; (keypad! stdscr #t)
+    ;; (noecho!)
+
+    ;; Xi init code
+    (xile-rpc-send port-to-xi (xile-msg-init '()))
+    (let ((msg (xile-msg-new_view '((file_path . "README.org")))))
+      (xile-register-callback
+       (car msg)
+       (lambda (result)
+         (format #t "Got ~a for the new_view message ~
+                     -> Now I'd like to put the value back into ~
+                        a parent environment binding~%" result)))
+      (xile-rpc-send port-to-xi msg))
+
+    ;; (addstr stdscr "Type any character to see it in bold\n")
+    ;; (let ((ch (getch stdscr)))
+    ;;   (addstr stdscr "The pressed key is ")
+    ;;   (if (char? ch)
+    ;;       (addch stdscr (bold ch))
+    ;;       (addchstr stdscr (bold (keyname ch))))
+
+    ;;   (refresh stdscr)
+    ;;   (getch stdscr)
+    ;;   (endwin))
+    ;; TODO : event loop thread instead of joining
+    (join-thread listener (+ 2 (current-time)))
+    (close-port port-to-xi)
+    (close-port port-from-xi)))
