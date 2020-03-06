4c170834898edf6279ff963cd51a06f3ff2133fb
Gerry Agbobada <gagbobada+git@gmail.com>
2020-03-06 16:46:40 +0100

    Use callback to print basic response to main window

--- xile.scm
+++ xile.scm
@@ -12,25 +12,37 @@
          (width (- (cols) 10))
          (startx 5)
          (starty 0))
-    (newwin height width starty startx)))
+    ((lambda (win)
+       (attr-set! win A_REVERSE)
+       (bkgdset! win (inverse #\sp))
+       win)
+     (newwin height width starty startx))))
 
 (define (make-xile-footer)
-  ;; TODO : 2 rows on the bottom
-  ;; top row reverse
-  ;; bottom row for the commands later
-  ;; return the footer window
   (let* ((height 2)
          (width 0)
          (startx 0)
          (starty (- (lines) height)))
-    (newwin height width starty startx)))
+    ((lambda (win)
+       (attr-set! win A_REVERSE)
+       (bkgdset! win (inverse #\sp))
+       win)
+     (newwin height width starty startx))))
+
+(define (update-header header-win text)
+      (addstr header-win text #:y 0 #:x 10)
+      (refresh header-win))
+
+(define (update-footer footer-win text)
+  (addstr footer-win text #:y 0 #:x (round (/ (- (cols) (string-length text)) 2)))
+  (refresh footer-win))
 
 (define (make-xile-main)
   ;; TODO : Get proper height/width params
   (let* ((height (- (lines) 2 1))
          (width 0)
          (startx 0)
-         (starty (- (lines) 1)))
+         (starty 1))
     (newwin height width starty startx)))
 
 ;; Main
@@ -44,6 +56,7 @@
 
     ;; Window init code
     (raw!)
+    (start-color!)
     (keypad! stdscr #t)
     (noecho!)
 
@@ -52,48 +65,48 @@
           (xile-main (make-xile-main))
           (xile-footer (make-xile-footer)))
 
+      (keypad! xile-header #t)
+      (keypad! xile-main #t)
+      (keypad! xile-footer #t)
 
-      ;; HACK Header and Footer hacks
-      (attr-set! xile-header A_REVERSE)
-      (bkgdset! xile-header (inverse #\sp))
-      (addstr xile-header "Xile alpha -- header" #:y 0 #:x 5)
-      (refresh xile-header)
+      ;; Header and Footer init
+      (update-header xile-header "Xile alpha -- header")
+      (update-footer xile-footer "Xile alpha -- footer")
 
-      (attr-set! xile-footer A_REVERSE)
-      (bkgdset! xile-footer (inverse #\sp))
-      (addstr xile-footer "Xile alpha -- footer" #:y 0 #:x (round (/ (- (cols) 20) 2)))
-      (refresh xile-footer)
-
-      ;; Logging init code
+      ;; HACK Logging init code
       (set-current-error-port (open "logs/xile-err.log" (logior O_APPEND O_CREAT O_WRONLY)))
       (set-current-output-port (open "logs/xile-out.log" (logior O_APPEND O_CREAT O_WRONLY)))
 
-      ;; Xi init code
+      ;; HACK Xi init code
       (xile-rpc-send port-to-xi (xile-msg-init '()))
       (let ((msg (xile-msg-new_view '((file_path . "README.org")))))
         (xile-register-callback
          (car msg)
          (lambda (result)
-           (format #t "Got ~a for the new_view message ~
+           ;; The output-port for this lambda is the xile-listen-err file-port
+           (addstr xile-main
+                   (format #f "Got ~a for the new_view message ~%~
                      -> Now I'd like to put the value back into ~
-                        a parent environment binding~%" result)))
+                        a parent environment binding~%" result)
+                   #:y 10 #:x 0)
+           (refresh xile-main)))
         (xile-rpc-send port-to-xi msg))
 
       ;; HACK Basic xile-main code
-      (addstr xile-main "Type any character to see it in bold\n" #:y 10 #:x 30)
+      (attr-set! xile-main A_NORMAL)
+      (addstr xile-main "Type any character to see it in bold" #:y 8 #:x 30)
       (refresh xile-main)
       (let ((ch (getch xile-main)))
-        (addstr xile-main "The pressed key is " #:y 11 #:x 30)
+        (addstr xile-main "The pressed key is " #:y 9 #:x 30)
         (if (char? ch)
             (addch xile-main (bold ch))
             (addchstr xile-main (bold (keyname ch))))
         (refresh xile-main))
 
-      (move xile-main 0 10)
+      ;; TODO : event loop thread instead of exiting just after this
       (getch xile-main))
 
     ;; Closing code (give back resources)
-    ;; TODO : event loop thread instead of joining
     (join-thread listener (current-time))
     (close-port port-to-xi)
     (close-port port-from-xi)
