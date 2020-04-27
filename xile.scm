7e0f23444913df3c0f52417d9c343adc76d77b41
Gerry Agbobada <gagbobada+git@gmail.com>
2020-04-28 00:59:09 +0200

    Refactor : move callbacks in their own module

--- xile.scm
+++ xile.scm
@@ -8,7 +8,8 @@
              (xile json-rpc)
              (xile curses-window)
              (xile backend-notifications)
-             (xile line-cache))
+             (xile line-cache)
+             (xile callbacks))
 
 ;; Main
 (define (main args)
@@ -48,55 +49,7 @@
       (xile-rpc-send port-to-xi send-mutex (xile-msg-init))
 
       (begin
-        ;; HACK register notification callback
-        (xile-register-callback
-         'update
-         ;; The "update" callback here is just badly extracting the text
-         ;; from the first line of updates.
-         (lambda (result)
-           (let* ((view_id (assoc-ref result "view_id"))
-                  (xile-buffer (find-xile-buffer (string->symbol view_id))))
-             (format #t "Update : xile-buffer for ~a is ~a~%" view_id xile-buffer)
-             (when xile-buffer
-               ((xile-buffer 'cb-update) (parse-xi-update result))))))
-
-        (xile-register-callback
-         'scroll_to
-         (lambda (result)
-           (let* ((y (assoc-ref result "line"))
-                 (x (assoc-ref result "col"))
-                 (view_id (assoc-ref result "view_id"))
-                 (xile-buffer (find-xile-buffer (string->symbol view_id))))
-             (format #t "Scroll-to : xile-buffer for ~a is ~a~%" view_id xile-buffer)
-             (when xile-buffer
-               ((xile-buffer 'cb-scroll-to) y x)))
-           ))
-
-        (xile-register-callback
-         'language_changed
-         (lambda (result)
-           (format #t "language_changed unimplemented !~%")))
-
-        (xile-register-callback
-         'config_changed
-         (lambda (result)
-           (format #t "config_changed unimplemented !~%")))
-
-        (xile-register-callback
-         'available_themes
-         (lambda (result)
-           (format #t "available_themes unimplemented !~%")))
-
-        (xile-register-callback
-         'available_plugins
-         (lambda (result)
-           (format #t "available_plugins unimplemented !~%")))
-
-        (xile-register-callback
-         'available_languages
-         (lambda (result)
-           (format #t "available_languages unimplemented !~%")))
-
+        (register-default-callbacks)
         ;; HACK open / manipulate file
         (define first-buffer (make-xile-buffer port-to-xi send-mutex "README.org"))
         ((first-buffer 'create-view))
---
+++ xile/callbacks.scm
@@ -0,0 +1,57 @@
+;; coding: utf-8
+
+(define-module (xile callbacks)
+  #:use-module (xile json-rpc)
+  #:use-module (xile curses-window)
+  #:use-module (xile backend-notifications)
+  #:export (register-default-callbacks))
+
+(define (register-default-callbacks)
+  "Register default callbacks on Xi incoming notifications / messages"
+  (begin
+    (xile-register-callback
+     'update
+     ;; The "update" callback here is just badly extracting the text
+     ;; from the first line of updates.
+     (lambda (result)
+       (let* ((view_id (assoc-ref result "view_id"))
+              (xile-buffer (find-xile-buffer (string->symbol view_id))))
+         (format #t "Update : xile-buffer for ~a is ~a~%" view_id xile-buffer)
+         (when xile-buffer
+           ((xile-buffer 'cb-update) (parse-xi-update result))))))
+
+    (xile-register-callback
+     'scroll_to
+     (lambda (result)
+       (let* ((y (assoc-ref result "line"))
+              (x (assoc-ref result "col"))
+              (view_id (assoc-ref result "view_id"))
+              (xile-buffer (find-xile-buffer (string->symbol view_id))))
+         (format #t "Scroll-to : xile-buffer for ~a is ~a~%" view_id xile-buffer)
+         (when xile-buffer
+           ((xile-buffer 'cb-scroll-to) y x)))))
+
+    (xile-register-callback
+     'language_changed
+     (lambda (result)
+       (format #t "language_changed unimplemented !~%")))
+
+    (xile-register-callback
+     'config_changed
+     (lambda (result)
+       (format #t "config_changed unimplemented !~%")))
+
+    (xile-register-callback
+     'available_themes
+     (lambda (result)
+       (format #t "available_themes unimplemented !~%")))
+
+    (xile-register-callback
+     'available_plugins
+     (lambda (result)
+       (format #t "available_plugins unimplemented !~%")))
+
+    (xile-register-callback
+     'available_languages
+     (lambda (result)
+       (format #t "available_languages unimplemented !~%")))))
