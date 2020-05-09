6cfa575c41179988f09f245582df64224c0e7625
Gerry Agbobada <gagbobada+git@gmail.com>
2020-05-09 19:46:40 +0200

    Add small tests for curses-window

---
+++ test/xile-curses-window-spec.scm
@@ -0,0 +1,30 @@
+#!/usr/bin/env -S guile --no-auto-compile -s
+!#
+
+(add-to-load-path (dirname (dirname (current-filename))))
+
+(use-modules (srfi srfi-64)
+             (xile curses-window))
+
+(test-begin "encode-keys")
+
+(test-equal "Encode simple letter"
+  "q"
+  (encode-key-to-string-sequence #\q))
+
+(test-equal "Encode shift letter"
+  "Q"
+  (encode-key-to-string-sequence #\Q))
+
+(test-equal "Encode special key"
+  "\\[DOWN]"
+  (encode-key-to-string-sequence KEY_DOWN))
+
+;; TODO : Find out what's sent on C-key and M-key
+;; (test-equal "Encode control letter"
+;;   "\\[C-d]"
+;;   (encode-key-to-string-sequence "^Q"))
+
+(test-end "encode-keys")
+
+(exit (+ (test-runner-fail-count (test-runner-get)) (test-runner-xpass-count (test-runner-get))))
